65c672053b1384ef5a09c2162c4fa29a2d7c5c93
Reid-Jourdan <71857615+Reid-Jourdan@users.noreply.github.com>
2023-02-08 11:53:11 -0600

    reformat intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,13 +4,9 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -68,7 +64,6 @@ public class Intake extends SubsystemBase {
       case Out:
         intakeSolenoid.set(true);
         break;
-
     }
   }
 }
