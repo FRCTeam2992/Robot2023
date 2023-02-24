0f340b28af5e8b5504860118ea47453f684bdd66
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2023-02-23 20:27:41 -0600

    Clean up whitespace

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -109,10 +109,10 @@ public class Robot extends TimedRobot {
     mRobotContainer.mDrivetrain.navx.zeroYaw();
 
     mRobotContainer.mDrivetrain.setDriveNeutralMode(NeutralMode.Brake);
-        mRobotContainer.mDrivetrain.setTurnNeutralMode(NeutralMode.Brake);
+    mRobotContainer.mDrivetrain.setTurnNeutralMode(NeutralMode.Brake);
 
-        mRobotContainer.mDrivetrain.setDriveCurrentLimit(40.0, 40.0);
-        mRobotContainer.mDrivetrain.setDriveRampRate(0.25);
+    mRobotContainer.mDrivetrain.setDriveCurrentLimit(40.0, 40.0);
+    mRobotContainer.mDrivetrain.setDriveRampRate(0.25);
   }
 
   /** This function is called periodically during operator control. */
