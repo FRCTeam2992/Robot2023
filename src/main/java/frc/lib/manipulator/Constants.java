d5c9b6fcfa34cbb22afec341543d237686c49729
Ryan Harvey <ryan.b.harvey@gmail.com>
2023-03-03 21:41:35 -0600

    Update manipulator lib constants to match measurements of no-fly and planned-path zones

--- src/main/java/frc/lib/manipulator/Constants.java
+++ src/main/java/frc/lib/manipulator/Constants.java
@@ -1,22 +1,28 @@
 package frc.lib.manipulator;
 
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Elevator;
+
 public final class Constants {
-        public static final double INCHES_MAX = 32.0;
-        public static final double DEGREES_MAX = 212.0;
+        public static final double MAX_HEIGHT_INCHES = Elevator.ElevatorPosition.HARD_STOP_TOP.positionInches;
+        public static final double MAX_ANGLE_DEGREES = Arm.ArmPosition.TOP_HARD_STOP.positionDegrees;
+        public static final double AVOID_HEIGHT_INCHES = 9.0;
+        public static final double BASE_AVOID_INCHES = 10.0;
+        public static final double BASE_AVOID_DEGREES = 90.0;
+        public static final double BASE_ENTRY_INCHES = 10.0;
+        public static final double BASE_ENTRY_DEGREES = Arm.ArmPosition.SPINDEXER_GRAB.positionDegrees;
+        public static final double CROSSBAR_ENTRY_INCHES = 3.0;
+        public static final double CROSSBAR_ENTRY_DEGREES = 180.0;
 
         public static final class NoFlyZones {
                 // Corner x, corner y, width, height
                 public static final NoFlyZone INSIDE_ROBOT = new NoFlyZone(6.0, 27.0, 9.0, 90.0);
-                public static final NoFlyZone ELEVATOR_CROSSBAR = new NoFlyZone(
-                                INCHES_MAX - 6.0, DEGREES_MAX - 10.0,
-                                INCHES_MAX - 3.0, DEGREES_MAX);
+                public static final NoFlyZone ELEVATOR_CROSSBAR = new NoFlyZone(3.0, 205.0, 9.0, MAX_ANGLE_DEGREES);
         }
 
         public static final class PlannedPathZones {
                 public static final NoFlyZone ROBOT_BASE = new NoFlyZone(0.0, 0.0, 9.0, 27.0);
-                public static final NoFlyZone ABOVE_CROSSBAR = new NoFlyZone(
-                                INCHES_MAX - 3.0, DEGREES_MAX - 10.0,
-                                INCHES_MAX, DEGREES_MAX);
+                public static final NoFlyZone BELOW_CROSSBAR = new NoFlyZone(0.0, 205.0, 3.0, MAX_ANGLE_DEGREES);
         }
 
         public static final class Boundaries {
@@ -25,17 +31,14 @@ public final class Constants {
                                 0.0, 6.0, 90.0);
                 public static final NoFlyBoundary BOUNDARY_CROSSBAR = new NoFlyBoundary(
                                 NoFlyBoundary.NoFlyBoundaryType.AT_HEIGHT,
-                                DEGREES_MAX - 10.0, DEGREES_MAX, INCHES_MAX - 3.0);
+                                MAX_ANGLE_DEGREES - 10.0, MAX_ANGLE_DEGREES, MAX_HEIGHT_INCHES - 3.0);
         }
 
         public static final class Waypoints {
-                public static final Waypoint WAYPOINT_BASE_ENTRY = new Waypoint(10.0, 15.0);
-                public static final Waypoint WAYPOINT_BASE_AVOID = new Waypoint(9.0, 90.0);
+                public static final Waypoint WAYPOINT_BASE_ENTRY = new Waypoint(BASE_ENTRY_INCHES, BASE_ENTRY_DEGREES);
+                public static final Waypoint WAYPOINT_BASE_AVOID = new Waypoint(BASE_AVOID_INCHES, BASE_AVOID_DEGREES);
                 public static final Waypoint WAYPOINT_CROSSBAR_ENTRY = new Waypoint(
-                                INCHES_MAX - 2.0,
-                                DEGREES_MAX - 10.0);
-                public static final Waypoint WAYPOINT_CROSSBAR_AVOID = new Waypoint(
-                                INCHES_MAX - 3.0,
-                                DEGREES_MAX - 10.0);
+                                CROSSBAR_ENTRY_INCHES,
+                                CROSSBAR_ENTRY_DEGREES);
         }
 }
